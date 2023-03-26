97e509d808a7f4d9a87bac3b082951184917e5ff
Murat Han Acıpayam <acipayammurathan@gmail.com>
2023-03-27 02:04:58 +0300

    weather repository impl

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,10 +1,20 @@
 package com.plcoding.weatherapp.data.mappers
 
 import com.plcoding.weatherapp.data.remote.WeatherDataDto
+import com.plcoding.weatherapp.data.remote.WeatherDto
 import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
+
+private data class IndexedWeatherData(
+    val index : Int,
+    val data : WeatherData
+){
+
+}
 
 fun WeatherDataDto.toWeatherDataMap() : Map<Int,List<WeatherData>>{
     return time.mapIndexed { index, time ->
@@ -13,15 +23,34 @@ fun WeatherDataDto.toWeatherDataMap() : Map<Int,List<WeatherData>>{
         val windSpeed = windSpeeds[index]
         val pressure = pressures[index]
         val humidity = humidities[index]
-        WeatherData(
-            time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
-            temperatureCelcius = temperature,
-            pressure = pressure,
-            windSpeed = windSpeed,
-            humidity  = humidity,
-            weatherType = WeatherType.fromWMO(weatherCode)
+        IndexedWeatherData(
+            index = index,
+            data = WeatherData(
+                time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
+                temperatureCelcius = temperature,
+                pressure = pressure,
+                windSpeed = windSpeed,
+                humidity  = humidity,
+                weatherType = WeatherType.fromWMO(weatherCode)
+            )
         )
     }.groupBy {
-        it.time.dayOfMonth
+        it.index / 24
+    }.mapValues {
+        it.value.map { it.data }
     }
+}
+
+fun WeatherDto.toWeatherInfo() : WeatherInfo{
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find {
+        val hour = if(now.minute < 30) now.hour else now.hour + 1
+        it.time.hour == hour
+    }
+
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
 }
---
+++ app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.plcoding.weatherapp.data.repository
+
+import com.plcoding.weatherapp.data.remote.WeatherApi
+import com.plcoding.weatherapp.domain.repository.WeatherRepository
+import com.plcoding.weatherapp.domain.util.Resource
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val api : WeatherApi
+)  : WeatherRepository{
+
+    override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
+        return try {
+
+        }catch (e : Exception){
+            e.printStackTrace()
+            Resource.Error(e.message ?: "An unknown error occured.")
+        }
+    }
+}
