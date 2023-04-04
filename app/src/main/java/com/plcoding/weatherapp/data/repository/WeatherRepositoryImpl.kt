1d497e1fab69e55650d378919a77c2dec1356446
Murat Han Acıpayam <acipayammurathan@gmail.com>
2023-04-04 03:16:49 +0300

    Getting Location Services Added

--- app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.plcoding.weatherapp.data.repository
 
+import com.plcoding.weatherapp.data.mappers.toWeatherInfo
 import com.plcoding.weatherapp.data.remote.WeatherApi
 import com.plcoding.weatherapp.domain.repository.WeatherRepository
 import com.plcoding.weatherapp.domain.util.Resource
@@ -12,7 +13,12 @@ class WeatherRepositoryImpl @Inject constructor(
 
     override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
         return try {
-
+            Resource.Success(
+                data = api.getWeatherData(
+                    lat = lat,
+                    long = long
+                ).toWeatherInfo()
+            )
         }catch (e : Exception){
             e.printStackTrace()
             Resource.Error(e.message ?: "An unknown error occured.")
