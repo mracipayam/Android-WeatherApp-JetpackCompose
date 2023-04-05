e8caf354f7637755fb8ce098452edcd2f86d02ac
Murat Han Acıpayam <acipayammurathan@gmail.com>
2023-04-06 02:10:34 +0300

    project done

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.plcoding.weatherapp.presentation
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
+import com.plcoding.weatherapp.presentation.ui.theme.DeepBlue
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel : WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher : ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
+
         setContent {
             WeatherAppTheme {
-                
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .background(DarkBlue)) {
+                    WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
+                }
             }
         }
     }
